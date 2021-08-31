e7def476ba5eb1337677fcda8ae023b0f09aa359
Tobias Olsson <tobias.olsson@pm.me>
2021-08-31 19:19:44 +0200

    Add navbar style

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,24 +1,33 @@
 import React from 'react';
-
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
 
 function Navbar() {
   return (
     <header>
-      <nav>
-        <Link to="/">
-          <h1>Outdoor</h1>
-        </Link>
+      <nav className={styles.navbar}>
         <ul>
           <li>
-            <Link to="/dam">Dam</Link>
+            <Link to="/">
+              <h1 className={styles.logo}>Outdoor</h1>
+            </Link>
           </li>
-          <li>
-            <Link to="/herr">Herr</Link>
+          <li className={styles.kategori}>
+            <NavLink activeClassName={styles.aktiv} to="/dam">
+              Dam
+            </NavLink>
           </li>
-          <li>
-            <Link to="/utrustning">Utrustning</Link>
+          <li className={styles.kategori}>
+            <NavLink activeClassName={styles.aktiv} to="/herr">
+              Herr
+            </NavLink>
           </li>
+          <li className={styles.kategori}>
+            <NavLink activeClassName={styles.aktiv} to="/utrustning">
+              Utrustning
+            </NavLink>
+          </li>
+          <li className={styles.varukorg}>Varukorg</li>
         </ul>
       </nav>
     </header>
