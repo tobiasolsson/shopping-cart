422aaf97816cf1db5fb93070913c2cc00cb53108
Tobias Olsson <tobias.olsson@pm.me>
2021-09-01 20:45:16 +0200

    Add shopping cart modal

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import styles from './Modal.module.css';
+
+const Modal = ({ handleClose, show, children }) => {
+  // show modal or hide depending on the className:
+  const showModal = show
+    ? `${styles.modal} ${styles.showModal}`
+    : `${styles.modal} ${styles.hideModal}`;
+
+  const showBackrop = show ? styles.backdrop : '';
+
+  return (
+    // TODO: Ta bort backdrop div till någon annanstans, eller lös problem med onClick={handleClose} (stänger även när jag klickar på modal)
+    <div className={showBackrop}>
+      <div className={showModal}>
+        <div>
+          {children}
+          <button type="button" onClick={handleClose}>
+            close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import Modal from '../modal/Modal';
 
 function Navbar() {
+  const [modal, setModal] = useState(false);
+
+  function modalOpen() {
+    setModal(true);
+  }
+
+  function modalClose() {
+    setModal(false);
+  }
+
   return (
     <header>
       <nav className={styles.navbar}>
@@ -27,9 +38,16 @@ function Navbar() {
               Utrustning
             </NavLink>
           </li>
-          <li className={styles.varukorg}>Varukorg</li>
+          <li className={styles.varukorg}>
+            <button type="button" onClick={modalOpen}>
+              Varukorg
+            </button>
+          </li>
         </ul>
       </nav>
+      <Modal show={modal} handleClose={modalClose}>
+        <h2>Hello Modal</h2>
+      </Modal>
     </header>
   );
 }
