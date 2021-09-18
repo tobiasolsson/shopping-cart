db86a5afa8db0a2763db5592432f468b4f95c8f3
Tobias Olsson <tobias.olsson@pm.me>
2021-09-18 14:02:36 +0200

    Fix cart styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -20,15 +20,33 @@ function Navbar({ shoppingCart }) {
       (currentSum, currentValue) => currentSum + currentValue,
       0,
     );
-    return sumTotal;
+
+    const totalPrice = `${sumTotal.toLocaleString('se')} kr`;
+
+    return totalPrice;
   }
 
   const itemsInCart = shoppingCart.map((item) => {
     const relevantInfo = Object.keys(item).filter(
-      (key) => key === 'title' || key === 'price',
+      (key) => key === 'title' || key === 'price' || key === 'image',
     );
-    const article = relevantInfo.map((key) => <li>{item[key]}</li>);
-    return <ul>{article}</ul>;
+    const article = relevantInfo.map((key) => {
+      if (key === 'image') {
+        return <img src={item[key]} alt="" className={styles.cartImage} />;
+      }
+      if (key === 'title') {
+        return <li className={styles.title}>{item[key]}</li>;
+      }
+      if (key === 'price') {
+        return (
+          <li className={styles.price}>
+            {`${item[key].toLocaleString('se')} kr`}
+          </li>
+        );
+      }
+      return <li>{item[key]}</li>;
+    });
+    return <ul className={styles.itemCart}>{article}</ul>;
   });
 
   return (
@@ -57,17 +75,18 @@ function Navbar({ shoppingCart }) {
           </li>
           <li className={styles.varukorg}>
             <button type="button" onClick={modalOpen}>
-              Varukorg
+              <span className={styles.numberItems}>{shoppingCart.length}</span>
+              <span className={styles.totalCart}>{calculateTotal()}</span>
             </button>
           </li>
         </ul>
       </nav>
       <CartModal show={modal} handleClose={modalClose}>
-        <h2>Hello Modal</h2>
+        <h2 className={styles.dinVarukorg}>Din varukorg</h2>
         {itemsInCart}
-        <ul>
-          <li>Summa:</li>
-          <li>{calculateTotal()}</li>
+        <ul className={styles.totalPrice}>
+          <li className={styles.summa}>Summa:</li>
+          <li className={styles.total}>{calculateTotal()}</li>
         </ul>
       </CartModal>
     </header>
