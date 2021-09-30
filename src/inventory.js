ec530c6e64ff883f2cfa9654330deb90a66f8e86
Tobias Olsson <tobias.olsson@pm.me>
2021-09-30 20:25:28 +0200

    Add final products to inventory

--- src/inventory.js
+++ src/inventory.js
@@ -11,9 +11,36 @@ import ovikNordicSweaterH from './assets/images/herr/ovik_nordic_sweater_m_82020
 import gaiterH from './assets/images/herr/gaiter/oragngeWaves.webp';
 import kaipakH from './assets/images/herr/kaipak/darkGreyBlack.webp';
 import karlH from './assets/images/herr/karl/clayBlue.webp';
+
 // Ladies
+// Jacket
+import ExpeditionXW from './assets/images/dam/jacket/expedition_x-latt_jacket_w_86334-662_a_main_fjr.webp';
+import kebEcoShellW from './assets/images/dam/jacket/keb_eco-shell_jacket_w.webp';
+import stinaJacketW from './assets/images/dam/jacket/stina_jacket_w_89234-342_a_main_fjr.webp';
+// Sweater
+import fjallravenLogoSweaterW from './assets/images/dam/sweater/fjallraven_logo_sweater.webp';
+import ovikKnitSweaterW from './assets/images/dam/sweater/ovik_knit_sweater.jpg';
+import visbySweaterW from './assets/images/dam/sweater/visby_sweater.jpg';
+// Pants
+import kebTrousersW from './assets/images/dam/pant/keb_trousers_w_short_89898s-357-410_a_main_fjr.webp';
+import karlaProTrousersW from './assets/images/dam/pant/karla_pro_trousers_w_89726-235_a_main_fjr.webp';
+import abiskoTrekkingTightsW from './assets/images/dam/pant/abisko_trekking_tights_w_89586-662_a_main_fjr.webp';
 
 // Gear
+// Backpack
+import kaipak28 from './assets/images/utrustning/backpack/7323450091248_ss18_a_kaipak_28_21.webp';
+import keb72 from './assets/images/utrustning/backpack/7323450489434_ss19_srrb_keb_72_fjaellraeven_21.jpg';
+import singi20 from './assets/images/utrustning/backpack/7323450522230_fw19_a_singi_20_fjaellraeven_21.webp';
+
+// Thermos
+import jaervBottle from './assets/images/utrustning/thermos/2300010094128_e_helags_laeder_jaerv_20.jpg';
+import thermos from './assets/images/utrustning/thermos/5010576853233_a_staltermos_1l_thermos_20.jpg';
+import stanley from './assets/images/utrustning/thermos/6939236347587_a_classic_bottle_19l_stanley_20.jpg';
+
+// Kitchen
+import mimerStove from './assets/images/utrustning/kitchen/7330033324662_a_mimer_stove_kit_primus_20.jpg';
+import primesEssential from './assets/images/utrustning/kitchen/7330033905519_a_essential_stove_set_13l_primus_20.jpg';
+import primeteckStove from './assets/images/utrustning/kitchen/7330033905533_b_primetech_stove_set_13l_primus_20.jpg';
 
 export const herr = [
   {
@@ -92,10 +119,150 @@ export const herr = [
 
 export const dam = [
   {
+    image: fjallravenLogoSweaterW,
+    category: 'top',
+    title: 'Fjällräven Logo Sweater',
+    price: 999,
+    color: 'Mesa Purple',
+    desc: 'Klassisk sweatshirt i härligt robust Felpa-jersey av ekobomull.',
+  },
+  {
+    image: ovikKnitSweaterW,
+    category: 'top',
+    title: 'Övik Knit Sweater',
+    price: 1799,
+    color: 'Arctic Green',
+    desc: 'Vackert mönsterstickad tröja i mjuk, varm ull. Rund halsringning, välsittande raglanskärning och förstärkning på armbågarna.',
+  },
+  {
+    image: visbySweaterW,
+    category: 'top',
+    title: 'Visby Sweater',
+    price: 2799,
+    color: 'Grey',
+    desc: 'Robust stickad tröja i svensk ull, perfekt för vardag och uteliv i kyligt väder.',
+  },
+  {
+    image: ExpeditionXW,
     category: 'jacket',
-    title: 'Jacka',
-    price: 1999,
+    title: 'Expedition X-Lätt Jacket',
+    price: 2299,
+    color: 'Deep Forest',
+    desc: 'Lätt förstärkningsjacka med syntetfyllning för vandring och friluftsliv, året runt.',
+  },
+  {
+    image: kebEcoShellW,
+    category: 'jacket',
+    title: 'Keb Eco-Shell Jacket ',
+    price: 5499,
+    color: 'Dark Navy',
+    desc: 'Högfunktionell trelagers skaljacka i stretchig Eco-Shell. Minimalistisk utformning med fast huva, lättillgängliga bröstfickor och ventilation i sidan.',
+  },
+  {
+    image: stinaJacketW,
+    category: 'jacket',
+    title: 'Stina Jacket',
+    price: 2199,
+    color: 'Raspberry Red',
+    desc: 'Skön och praktisk vardagsjacka i slitstark G-1000 Original och G-1000 Lite. Fast huva och två handfickor.',
+  },
+  {
+    image: abiskoTrekkingTightsW,
+    category: 'pants',
+    title: 'Abisko Trekking Tights',
+    price: 1899,
+    color: 'Deep Forest',
+    desc: 'Tekniska vandringstights i tåligt och högfunktionellt stretchmaterial. Praktiska fickor och förstärkningar som ger extra stryktålighet i tuff terräng.',
+  },
+  {
+    image: karlaProTrousersW,
+    category: 'pants',
+    title: 'Karla Pro Trousers',
+    price: 1699,
     color: 'blue',
-    desc: 'Varm parkas',
+    desc: 'Slitstark friluftsbyxa med Regular fit i borstad G-1000 Silent. Teknisk skärning ger extra rörelsefrihet och det finns sex praktiska fickor.',
+  },
+  {
+    image: kebTrousersW,
+    category: 'pants',
+    title: 'Keb Trousers',
+    price: 2299,
+    color: 'Port-Mesa Purple',
+    desc: 'Teknisk vandringsbyxa i stretch och G-1000 Eco, perfekt för långa dagar ute på fjället.',
+  },
+];
+
+export const utrustning = [
+  {
+    image: kaipak28,
+    category: 'backpack',
+    title: 'Kaipak 28',
+    price: 1799,
+    color: 'Stoen Grey',
+    desc: 'Avskalad, robust vandringsryggsäck i G-1000 HeavyDuty Eco. Toppmatat huvudfack, frontficka och avtagbar midjerem.',
+  },
+  {
+    image: keb72,
+    category: 'backpack',
+    title: 'Keb 72',
+    price: 3699,
+    color: 'Olive-Deep Forest',
+    desc: 'Ergonomisk, rejäl fyra säsongers ryggsäck för långa turer till fots och på skidor.',
+  },
+  {
+    image: singi20,
+    category: 'backpack',
+    title: 'Singi 20',
+    price: 1099,
+    color: 'Cabin Red',
+    desc: 'Slitstark dagtursryggsäck. Praktisk och flexibel för vandring, jakt och fiske.',
+  },
+  {
+    image: jaervBottle,
+    category: 'bottle',
+    title: 'JÄRV Outdoor HELAGS LÄDER 0,58L',
+    price: 649,
+    color: 'Ljus Läder',
+    desc: 'HELAGS är tillverkad i vakuum-isolerat rostfritt stål av hög kvalitet (18/8). Tar inte upp smaker, håller din dryck varm i 8 timmar och kall i 24 timmar.',
+  },
+  {
+    image: thermos,
+    category: 'bottle',
+    title: 'Thermos STÅLTERMOS 1L',
+    price: 399,
+    color: 'Rostfritt stål',
+    desc: 'Ståltermos av absolut högsta kvalitet. Tillverkad i rostfritt fjäderstål med unik patenterad vakuumiseringsteknik. Patenterad hygienisk autokork som är delbar för enkel rengöring.',
+  },
+  {
+    image: stanley,
+    category: 'bottle',
+    title: 'Stanley THE LEGENDARY CLASSIC BOTTLE 1,9L',
+    price: 799,
+    color: 'Hammertone Green',
+    desc: 'Klassisk grön termos från Stanley som rymmer ordentligt med kaffe! Håller din dryck varm i upp till 45 timmar och kall i upp till 2 dagar. Den är läckage- och diskmaskinssäker.',
+  },
+  {
+    image: mimerStove,
+    category: 'kitchen',
+    title: 'Primus MIMER STOVE KIT - Gaskök',
+    price: 599,
+    color: 'Aluminium',
+    desc: 'Smidigt friluftskök med brännare, två kastruller, stekpanna och griptång. Det är pålitligt och lättanvänt, vilket gör att det passar perfekt till familjeutflykten, bil- eller båtturen.',
+  },
+  {
+    image: primesEssential,
+    category: 'kitchen',
+    title: 'Primus ESSENTIAL STOVE SET 1.3L - Gaskök',
+    price: 1199,
+    color: 'Aluminium',
+    desc: '  Kompakt och stadigt gaskök med brännare, två kastruller och en stekpanna. Passar perfekt för dig som enkelt vill laga mat ute i det fria, oavsett om du ska på picknick eller ut på en längre vandring.',
+  },
+  {
+    image: primeteckStove,
+    category: 'kitchen',
+    title: 'Primus PRIMETECH STOVE SET 1.3L - Gaskök',
+    price: 1699,
+    color: 'Hårdanodiserat aluminium',
+    desc: 'Effektivt och stabilt gaskök med integrerad brännare i vindskyddet. En kastrull med värmeväxlare och keramisk insida, en kastrull i hårdanodiserat aluminium och lock med integrerat durkslag.',
   },
 ];
